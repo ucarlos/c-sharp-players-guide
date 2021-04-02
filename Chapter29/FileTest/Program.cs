1a9dccc6bc8f53aec8dd55ad921ba9e972cf9c94
Ulysses Carlos <ulysses_carlos@protonmail.com>
2021-04-02 16:28:36 -0400

    Added Chapter29

---
+++ Chapter29/FileTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace Chapter29 {
+    class Program {
+        static void Main(string[] args) {
+            string input_filename = "../../../Input.txt";
+            string output_filename = "../../../Output.txt";
+
+            string[] line_list = File.ReadAllLines(input_filename);
+            
+            FileStream fs = File.OpenWrite(output_filename);
+            // Manually Truncate the file because File.OpenWrite Doesn't truncate the file first.
+            fs.SetLength(0);
+            StreamWriter streamWriter = new StreamWriter(fs);
+            
+            // Write last two lines to Example.txt:
+            if (line_list.LongLength >= 2) {
+                Console.WriteLine($"Writing last two lines of \"{input_filename}\" to \"{output_filename}\"");
+                var end = line_list.LongLength - 1;
+                streamWriter.WriteLine(line_list[end]);
+                streamWriter.WriteLine(line_list[end - 1]);
+            }
+            else
+                streamWriter.WriteLine($"Sorry, \"{input_filename}\" is empty.");
+            
+
+            streamWriter.Close();
+            fs.Close();
+            Console.WriteLine("Complete!");
+        }
+    }
+}
